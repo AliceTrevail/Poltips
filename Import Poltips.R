cfb16a4f4a848d7238d97ef114b407a43a828443
Alice Trevail <alice.trevail@liv.ac.uk>
2018-11-15 10:11:51 +0000

    Create Import Poltips.R

---
+++ Import Poltips.R
@@ -0,0 +1,50 @@
+### Import Poltips text file data to R ###
+
+###### install and load packages ########
+
+if(!"dplyr" %in% installed.packages())
+  install.packages("dplyr")
+library(dplyr)
+
+if(!"readr" %in% installed.packages())
+  install.packages("readr")
+library(readr)
+
+###### functions ########
+
+# function to open text file saved from poltips software of high and low waters
+import.POLTIPS <- function(x){
+  
+  # read in text file
+  a <- read_delim(x, " ",                                                  # read in .txt file x, deliminated by white space
+                  col_names = FALSE,                                       # dont use first row as column names
+                  col_types = cols(X2 = col_skip(), X3 = col_character(),  # skip column X2, read column X3 (time) as character
+                                   X4 = col_skip(), X5 = col_skip(),       # skip columns X4 and X5
+                                   X6 = col_skip(), X7 = col_number()),    # skip column X6, read column X7 (tide height) as number
+                  skip = 1)                                                # skip row one of plain text
+  
+  # convert to relevant format
+  b <- as.data.frame(a)                         # convert to data frame
+  colnames(b) <- c("Date", "Time", "Height")    # give relevant column names
+  
+  # name tides by high or low water. Poltips generates data for sequential tides so can use binary nature of data
+  b$Tide <- "NA"                                                  # create empty column of tide type
+  b$Tide[1] <- ifelse(b$Height[1] < b$Height[2], "LW", "HW")      # for row one, if height is lower than row 2 = "LW", else "HW"
+  for (i in 2:NROW(b)){                                           # loop for remaining rows
+    b$Tide[i] <- ifelse(b$Height[i] < b$Height[i-1], "LW", "HW")  # if height lower than row above = "LW", else "HW"
+  }
+  
+  # return new data frame
+  b
+}
+
+# function to return nearest time between date x and high water dataframe
+nearest.time <- function(x, y, key = "posixct"){y[which.min(abs(difftime(x, y[,key]))),key]}
+
+
+### **************************************** ##########
+
+### Import data #####
+
+# import tide data
+Tide_Pu2010 <- import.POLTIPS("E:/Data/Tide/PuffinTideTimes2010.txt")
